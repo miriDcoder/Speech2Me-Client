817647179bd5d348703ac6d614ff5ef0594b0c85
miril <miri.levi94@gmail.com>
2019-04-20 18:00:30 +0300

    Added: *Signup page *Db utils class - for any database code
    Modified:
    *added constructors for teacher & student
    
    ->check the TODOs

---
+++ app/src/main/java/com/example/project/DbUtils.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+public class DbUtils {
+
+    /*
+    TODO:
+    1. insert to db- all functions retrieves the id they got from db
+    1.1 teacher
+    1.2 student
+    1.3 student to teacher
+    2. check if given teacher id is in db
+    */
+}
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -65,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-
+                startActivity(new Intent(MainActivity.this, SignUp.class));
             }
         });
     }
