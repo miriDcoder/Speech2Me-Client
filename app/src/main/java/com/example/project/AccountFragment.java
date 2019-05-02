c9b369c7a1ac5172b44af31b5ab2153daafcc6b1
miriDcoder <37478344+miriDcoder@users.noreply.github.com>
2019-05-02 17:29:37 +0300

    *Worked on Account activity - validating relevant fields. need to update the db.

--- app/src/main/java/com/example/project/AccountFragment.java
+++ app/src/main/java/com/example/project/AccountFragment.java
@@ -2,14 +2,17 @@ package com.example.project;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.Toast;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -26,35 +29,85 @@ public class AccountFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_account, container, false);
         final EditText editTextFirstName = (EditText)v.findViewById(R.id.editTextAccountFirstName);
-        EditText editTextLastName = (EditText)v.findViewById(R.id.editTextAccountLastName);
-        EditText editTextEmail = (EditText)v.findViewById(R.id.editTextAccountMail);
+        final EditText editTextLastName = (EditText)v.findViewById(R.id.editTextAccountLastName);
+        final EditText editTextEmail = (EditText)v.findViewById(R.id.editTextAccountMail);
         EditText editTextCity = (EditText)v.findViewById(R.id.editTextAccountCity);
+        final EditText editTextCurrPassword = (EditText)v.findViewById(R.id.editTextAccountCurrPassword);
+        final EditText editTextNewPassword = (EditText)v.findViewById(R.id.editTextAccountNewPassword);
+        final EditText editTextNewPasswordAgain = (EditText)v.findViewById(R.id.editTextAccountNewPasswordAgain);
+        Button buttonSaveChanges = (Button)v.findViewById(R.id.buttonSaveChanges);
         //-----------------NEED TO CHANGE AFTER CONNECTING REAL DB-------------
         ArrayList<User> users = tempDb.makeUserList();
         mUser = users.get(3);
     //----------------------------------------------------------------------
-    updateFieldsFromDb(editTextFirstName, editTextLastName, editTextEmail, editTextCity);
-        /*editTextFirstName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        updateFieldsFromDb(editTextFirstName, editTextLastName, editTextEmail, editTextCity);
+        InputFilter filter = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                for (int i = start;i < end;i++) {
+                    if (!Character.isLetter(source.charAt(i)) &&
+                            !Character.toString(source.charAt(i)).equals("-") &&
+                            !Character.toString(source.charAt(i)).matches("[\\u0590-\\u05ff]"))
+                    {
+                        //display toast
+                        return "";
+                    }
+                }
+
+                return null;
+            }
+        };
+        editTextFirstName.setFilters(new InputFilter[] {filter});
+        editTextLastName.setFilters(new InputFilter[] {filter});
+        editTextCity.setFilters(new InputFilter[] {filter});
+
+        editTextEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if(!hasFocus)
                 {
-                    if(!AppUtils.IsLetters(editTextFirstName.getText().toString()))
+                    if(!AppUtils.IsValidMail(editTextEmail.getText().toString()))
                     {
-                        editTextFirstName.setText("");
-                        messageToUser("אנא הזן שם תקין");
+                        editTextEmail.setText("");
+                        messageToUser("אנא הזן מייל תקין");
                     }
                 }
             }
-        });*/
+        });
+
+        buttonSaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isChangePassword = false;
+                String currPass = editTextCurrPassword.getText().toString();
+                if(!currPass.isEmpty())
+                {
+                    int compareCurrPass = currPass.compareTo(mUser.getmPassword());
+                    if(compareCurrPass != 0)
+                    {
+                        messageToUser("ססמה נוכחית שגויה.");
+                    }
+                    else {
+                        String newPass = editTextNewPassword.getText().toString();
+                        String newPassAgain = editTextNewPasswordAgain.getText().toString();
+                        int compare = newPass.compareTo(newPassAgain);
+                        if (!newPass.isEmpty() && !newPassAgain.isEmpty() && compare == 0) {
+                            //TODO: update new password in database
+                        } else {
+                            messageToUser("ססמה חדשה שגויה. יש להזין את אותה הססמה בשדות המתאימים.");
+                        }
+                    }
+                }
+
+                //TODO: UPDATE FIELDS IN DB
+            }
+        });
 
         return v;
     }
 
     private void updateFieldsFromDb(EditText iFirstName, EditText iLastName, EditText iEmail, EditText iCity)
     {
-
-
         iFirstName.setText(mUser.getmFirstName());
         iLastName.setText(mUser.getmLastName());
         iEmail.setText(mUser.getmEmail());
@@ -63,41 +116,12 @@ public class AccountFragment extends Fragment {
             iCity.setText(mUser.getmCity());
         }
     }
-//
-//    private void messageToUser(CharSequence text)
-//    {
-//        Context context = getApplicationContext();
-//        int duration = Toast.LENGTH_SHORT;
-//        Toast toast = Toast.makeText(context, text, duration);
-//        toast.show();
-//    }
 
-    /*TextWatcher NameValidatorTextWatcher = new TextWatcher() {
-        @Override
-        public void afterTextChanged(Editable s) {
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            if (filterLongEnough()) {
-                populateList();
-            }
-        }
-
-        private boolean filterLongEnough() {
-            return tv_filter.getText().toString().trim().length() > 2;
-        }
-    };*/
-    /*
-    TODO:
-    -->get USER from last activity
-    stopped at: key change event, to validate
-    1. display existing details
-    2. validate new details that the user entered
-    3. if all valid - update in db.
-    */
+    private void messageToUser(CharSequence text)
+    {
+        Context context = getActivity();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
--- app/src/main/java/com/example/project/AppUtils.java
+++ app/src/main/java/com/example/project/AppUtils.java
@@ -1,6 +1,12 @@
 package com.example.project;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AppUtils {
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     public static boolean IsLetters(String str)
     {
@@ -15,5 +21,11 @@ public class AppUtils {
         }
 
         return isAllLetters;
+    }
+
+    public static boolean IsValidMail(String str)
+    {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(str);
+        return matcher.find();
     }
 }
